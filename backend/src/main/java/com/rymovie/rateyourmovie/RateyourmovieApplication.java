c352e4c399d9eede5344ba77f6872fc2b367a806
Diego-Sant <diego_santana@unigranrio.br>
2022-07-18 19:30:06 -0300

    Commit1

---
+++ backend/src/main/java/com/rymovie/rateyourmovie/RateyourmovieApplication.java
@@ -0,0 +1,13 @@
+package com.rymovie.rateyourmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateyourmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateyourmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rymovie/rateyourmovie/RateyourmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rymovie.rateyourmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateyourmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
