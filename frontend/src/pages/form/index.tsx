04296f77d44baaba7ff138bb645377ea326ea599
Diego-Sant <diego_santana@unigranrio.br>
2022-07-18 23:23:28 -0300

    navbutton

--- frontend/src/pages/form/index.tsx
+++ frontend/src/pages/form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function Form() {
@@ -44,7 +45,9 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary rymovie-btn mt-3">Cancelar</button>
+        <Link to="/">
+          <button className="btn btn-primary rymovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
